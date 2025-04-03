01eb78f5e317fb95b2b168267957cfbf7bd4954c
MariaEdPaixao <mariaeduardaalvesdapaixao0807@gmail.com>
2025-04-03 11:58:39 -0300

    Melhorias no validation handler

--- src/main/java/br/com/fiap/fin_money_api/config/CorsConfig.java
+++ src/main/java/br/com/fiap/fin_money_api/config/CorsConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+// um bean é um objeto que é instanciado, configurado e gerenciado pelo contêiner Spring IoC
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
--- src/main/java/br/com/fiap/fin_money_api/exception/ValidationHandler.java
+++ src/main/java/br/com/fiap/fin_money_api/exception/ValidationHandler.java
@@ -2,21 +2,30 @@ package br.com.fiap.fin_money_api.exception;
 
 import java.util.List;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+// capturar os erros e devolve uma resposta, no formato json
+// quando falha uma validação do BeenValidation, ele retorna um erro 400 com o json 
+@RestControllerAdvice 
 public class ValidationHandler {
-    record ValidationError (String field, String message) {}
+    record ValidationError (String field, String message) {
+
+        public ValidationError(FieldError error) {
+           this(error.getField(), error.getDefaultMessage());
+        }}
 
     @ExceptionHandler(exception = MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public List<ValidationError> handler(MethodArgumentNotValidException e){
         return e.getFieldErrors()
                 .stream()
-                .map(error -> new ValidationError(error.getField(), error.getDefaultMessage()))
+                .map(ValidationError::new) //método de referencia 
                 .toList();
-    }
+    } //pegar só o nome do campo e a mensagem de erro 
 }
+
+//exception tem um atributo field errors
